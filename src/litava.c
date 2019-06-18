32dd20df1b948c1f1d0602046e5b289aa963a9b4
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-18 09:51:19 +0200

    Command line flags: -v, --version, -h, --help

--- src/litava.c
+++ src/litava.c
@@ -1,10 +1,15 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "pixmap.h"
 #include "bmp_reader.h"
 #include "bmp_writer.h"
 #include "hercule.h"
+
+int flag_specified(const char *short_flag, const char *long_flag, const char *arg) {
+    return strcmp(short_flag, arg) == 0 || strcmp(long_flag, arg) == 0;
+}
 
 int main(int argc, char **argv) {
     Pixmap *src = NULL;
@@ -12,6 +17,15 @@ int main(int argc, char **argv) {
     Pixmap *dest1 = NULL;
     int status = -1;
 
+    if (argc == 2) {
+        if (flag_specified("-v", "--version", argv[1])) {
+            puts("Litava version 1.0");
+            return 0;
+        } else if (flag_specified("-h", "--help", argv[1])) {
+            puts("Usage ./litava tested_image.bmp pattern_to_find.bmp output_image.bmp");
+            return 0;
+        }
+    }
     if (argc < 4) {
         puts("Usage ./litava tested_image.bmp pattern_to_find.bmp output_image.bmp");
         return 1;
